896bd92e239d2e22bf8fd50d6baed5fb5eed253d
Godsider <vdovgan.ua@gmail.com>
2018-10-28 09:11:47 +0200

    RC1

--- src/main.js
+++ src/main.js
@@ -30,11 +30,6 @@ const arrays = [
   []
 ]
 
-// checking if value isn't NaN/undefined/null
-// const isNumber = function(value) {
-//   return !!value || (value === 0)
-// }
-
 // performing the processing
 const process = function() {
   var resultStr = '<h3>The result:</h3>'
@@ -43,26 +38,9 @@ const process = function() {
   for (i = 0; i < arrays.length; i++) { // this loop iterates through arrays
     var numsSum = maxNumber = minNumber = none, j = 0, elemNow
     resultStr += 'Array ' + i + ': ' + strings[i]
-//     arrayNow = arrays[i]
-//     arrayLength = arrayNow.length
-//     arrayLength = arrays[i].length
-//             console.log('arrayLength: ' + arrayLength)
-
-    // setting an initial value for maxNumber and minNumber, avoiding NaN/undefined/null (if any) from the very beginning of the array
-    /*while (j < arrayLength) {
-              console.log(j)
-      if (isNumber(arrayNow[j])) {
-        maxNumber = minNumber = arrayNow[j]
-        break
-      }
-      j++ // increment if no break
-    }*/
 
     do { // this loop iterates through the specific array being processed right now
-//               console.log(j)
-//       elemNow = arrayNow[j]
       elemNow = arrays[i][j]
-//       if (isNumber(elemNow)) { // if the current element isn't NaN/undefined/null -- init vars or process the value
       if (!!elemNow || (elemNow === 0)) { // if the current element isn't NaN/undefined/null -- init vars or process the value
         maxNumber = (maxNumber === none) || (elemNow > maxNumber) ? elemNow : maxNumber
         minNumber = (minNumber === none) || (elemNow < minNumber) ? elemNow : minNumber
@@ -70,7 +48,6 @@ const process = function() {
       }
       j++
     } while (j < arrays[i].length)
-//     } while (j < arrayLength)
 
     resultStr += '<br>max = ' + maxNumber +
       '<br>min = ' + minNumber +
@@ -80,8 +57,5 @@ const process = function() {
   return resultStr
 }
 
-// console.log(resultStr)
-
 // injecting the result into the document
 document.getElementById("result").innerHTML = process()
-// alert('Run 5')
